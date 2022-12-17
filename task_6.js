1072af2d8f553e522fc73e2869d219349704b6e4
Roman <romasananas@gmail.com>
2022-12-17 14:42:38 +0300

    changes

--- task_6.js
+++ task_6.js
@@ -1,8 +1,9 @@
 let arr = [1,1,1,1,1];
-let result = arr.map(function(i,n,a) {
-    if (check === arr.indexOf(i)) {
+for (let i = 1; i < arr.length; ++i) {
+    if (arr[i - 1] == arr[i]) {
         console.log('true');
-    } else {
+    }
+    else {
         console.log('false');
     }
-});
+}
---
+++ task_7.js
@@ -0,0 +1,14 @@
+let arr = [1,2,3,4,5,6,7,8];
+let arrEven = [];
+let arrOdd = [];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 = 0) {
+        arrEven.push(arr[i]);
+    } else if (arr[i] % 2 != 0) {
+        arrOdd.push(arr[i]);
+    } else {
+        return none;
+    }
+}
+console.log(arrEven.length);
+console.log(arrOdd.length);
